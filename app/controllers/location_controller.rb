9dc9a0590f08f42e4726087254595948c3c0f292
Victor Gervacio <victorgervacio33@gmail.com>
2020-09-12 10:39:48 -0600

    location update

---
+++ app/controllers/location_controller.rb
@@ -0,0 +1,13 @@
+class LocationController < ApplicationController
+  def create
+  end
+
+  def read
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'location/create'
+  get 'location/read'
+  get 'location/update'
+  get 'location/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
