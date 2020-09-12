2fa2a5677e4de78dc74f7b1013d617f063adeb72
Scott Mohorn <scottmohorn@gmail.com>
2020-09-12 10:32:49 -0600

    trips model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+  get 'trips/show'
+  get 'trips/new'
+  get 'trips/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
